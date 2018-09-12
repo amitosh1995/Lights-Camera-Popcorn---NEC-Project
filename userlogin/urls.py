af5c071a6290839903595ba52549b5bc6bb8502d
Amitosh Kumar <k.amitosh@gmail.com>
2018-09-12 12:59:43 +0530

    Initial commit

---
+++ userlogin/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.sign_in, name='sign_in'),
+    path('post_sign_in/', views.post_sign_in, name='postSignIn')
+]
---
+++ userlogin/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import pyrebase
+
+
+config = {
+    'apiKey': "AIzaSyBnLexI4Z6Qt8FxVKpkYDK5hOBwTcPlqe4",
+    'authDomain': "lights-camera-popcorn-nec.firebaseapp.com",
+    'databaseURL': "https://lights-camera-popcorn-nec.firebaseio.com",
+    'projectId': "lights-camera-popcorn-nec",
+    'storageBucket': "lights-camera-popcorn-nec.appspot.com",
+    'messagingSenderId': "167484483918"
+}
+fireBase = pyrebase.initialize_app(config)
+
+auth = fireBase.auth()
+
+
+def sign_in(request):
+    return render(request, "userlogin/signIn.html")
+
+
+def post_sign_in(request):
+    email = request.POST.get('email')
+    passw = request.POST.get('pass')
+    try:
+        user = auth.sign_in_with_email_and_password(email, passw)
+    except:
+        message = "Invalid Username or password. Please try again :) !"
+        return render(request, "userlogin/signIn.html", {'msg': message})
+
+    return render(request, "userlogin/welcome.html", {'e': email})
