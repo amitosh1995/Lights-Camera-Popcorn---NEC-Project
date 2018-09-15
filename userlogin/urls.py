7ad33a078ae923534ad58c2e0a9d4f4a3a558e70
Amitosh Kumar <k.amitosh@gmail.com>
2018-09-16 01:40:45 +0530

    homepage and seat booking UI added

--- userlogin/urls.py
+++ userlogin/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.sign_in, name='sign_in'),
-    path('home/', views.post_sign_in, name='postSignIn'),
-    path('registration/', views.sign_up , name='sign_up')
+    path('home/', views.post_sign_in, name='post_Sign_In'),
+    path('registration/', views.sign_up, name='sign_up'),
+    path('registration/post_sign_up/', views.post_sign_up, name='post_Sign_Up'),
+    path('bookseats/', views.seat_booking, name='seat_selection')
 ]
--- userlogin/views.py
+++ userlogin/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import auth
 import pyrebase
 
 
@@ -12,7 +13,8 @@ config = {
 }
 fireBase = pyrebase.initialize_app(config)
 
-auth = fireBase.auth()
+authe = fireBase.auth()
+database = fireBase.database()
 
 
 def sign_in(request):
@@ -23,13 +25,39 @@ def post_sign_in(request):
     email = request.POST.get('email')
     passw = request.POST.get('pass')
     try:
-        user = auth.sign_in_with_email_and_password(email, passw)
+        user = authe.sign_in_with_email_and_password(email, passw)
     except:
         message = "Invalid Username or password. Please try again :) !"
         return render(request, "userlogin/signIn.html", {'msg': message})
-
+    print(user)
+    session_id = user['idToken']
+    request.session['uid'] = str(session_id)
     return render(request, "userlogin/welcome.html", {'e': email})
 
 
 def sign_up(request):
     return render(request, "userlogin/signUp.html", request.GET)
+
+
+def post_sign_up(request):
+    name = request.POST.get('username')
+    email = request.POST.get('email')
+    contact_no = request.POST.get('telephone')
+    passw = request.POST.get('pass')
+
+    user = authe.create_user_with_email_and_password(email, passw)
+
+    uid = user['localId']
+    data = {"Name": name, "Contact No": contact_no, "Email": email, "Password": passw}
+    database.child("Users").child(uid).child("User info").set(data)
+    message = "Registration Completed. Login to Book Tickets"
+    return render(request, "userlogin/signIn.html", {'msg': message})
+
+
+def log_out(request):
+    auth.logout(request)
+    return render(request, "userlogin/signIn.html")
+
+
+def seat_booking(request):
+    return render(request, "userlogin/bookseats.html")
